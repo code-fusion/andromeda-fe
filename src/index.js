f58a8e7a56ac51e6e6e92e1d0dff56bf995f4c91
Maximiliano Atanasio <maximiliano.atanasio@softvision.com>
2018-09-21 16:30:44 -0300

    Redux-persist implemented

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import {render} from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import './reset.css';
 import './index.scss';
 import App from './App';
 
-import store from './store';
+import {store, persistor} from './store';
 
 const rootElement = document.getElementById('root');
 
 render(
 	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<PersistGate loading={null} persistor={persistor}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</PersistGate>
 	</Provider>
 	, rootElement);
 
